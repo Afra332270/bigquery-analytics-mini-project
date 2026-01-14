d22a64e26fd1dca48d36696e2d143cffeaf70a6d
Afra332270 <afrarahman11@gmail.com>
2026-01-14 20:48:42 +0600

    Create 00_initial_setup.sql

---
+++ sql/00_initial_setup.sql
@@ -0,0 +1,38 @@
+ -- Creating our own dataset
+
+CREATE SCHEMA IF NOT EXISTS analytics_demo;
+
+
+-- Creating a small base table inside our dataset
+
+CREATE TABLE analytics_demo.trends_base AS
+SELECT *
+FROM `bigquery-public-data.google_trends.international_top_terms`
+WHERE refresh_date BETWEEN '2023-01-01' AND '2023-01-31'
+  AND country_name IN (
+    'India', 'Bangladesh', 'Pakistan',
+    'United States', 'United Kingdom'
+  );
+
+
+-- Creating an unpartitioned table using data from the base table
+
+CREATE TABLE analytics_demo.trends_unpartitioned AS
+SELECT * FROM analytics_demo.trends_base;
+
+
+-- Creating a partitioned table using data from the base table, partitioned by 'refresh_date'
+
+CREATE TABLE analytics_demo.trends_partitioned
+PARTITION BY DATE(refresh_date) AS
+SELECT * FROM analytics_demo.trends_base;
+
+
+-- Creating a partitioned and clustered table using data from the base table
+
+CREATE TABLE analytics_demo.trends_partitioned_clustered
+PARTITION BY DATE(refresh_date)
+CLUSTER BY country_name, term AS
+SELECT * FROM analytics_demo.trends_base;
+
+
